18efaed9eab70dc0ce8c72e0e41cc869084d2a0a
Raymart De Guzman <raymart54@gmail.com>
2018-02-07 19:59:17 -0500

    #12 removed onClick unused method name 'changeNickname'

--- src/org/thoughtcrime/securesms/webrtc/ChangeNickname.java
+++ src/org/thoughtcrime/securesms/webrtc/ChangeNickname.java
@@ -6,13 +6,18 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
 
 import org.thoughtcrime.securesms.R;
 
-public class ChangeNickname extends Activity implements View.OnClickListener{
+public class ChangeNickname extends Activity implements OnClickListener {
     private Button button1;
     final Context context = this;
 
+    @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipient_preference_activity);
@@ -22,9 +27,25 @@ public class ChangeNickname extends Activity implements View.OnClickListener{
     }
 
 
+    @Override
     public void onClick(View view){
         final Dialog dialog = new Dialog(context);
-        
+        dialog.setContentView(R.layout.add_nickname_dialog);
+        dialog.setTitle("Title...");
+
+        TextView txt = (TextView) dialog.findViewById(R.id.new_text);
+        txt.setText("Android custom dialog example");
+        ImageView image = (ImageView) dialog.findViewById(R.id.image);
+//        image.setImageResource(R.drawable.);
+
+        Button dialogButtonOk = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        dialogButtonOk.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
 
     }
 }
