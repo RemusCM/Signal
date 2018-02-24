59345f49a324ad18226b15faa2330a502da8e9d2
unknown <glasslake514@gmail.com>
2018-02-23 19:12:07 -0500

    Added tests for getMessageIdsByRecipientId, getMessageCountByRecipientId, and deleteAllMessagesByRecipientId.

--- test/unitTest/java/org/thoughtcrime/securesms/MessageMocking.java
+++ test/unitTest/java/org/thoughtcrime/securesms/MessageMocking.java
@@ -11,24 +11,33 @@ import org.thoughtcrime.securesms.database.SmsDatabase;
 import org.thoughtcrime.securesms.database.ThreadDatabase;
 import org.thoughtcrime.securesms.recipients.Recipient;
 
+import java.util.ArrayList;
+
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({DatabaseFactory.class})
-public class MessageMocking extends BaseUnitTest{
+public class MessageMocking extends BaseUnitTest {
 
     protected DatabaseFactory dbFactory;
     protected SmsDatabase smsDb;
     protected ThreadDatabase threadDb;
     protected Recipient recipient;
     protected Address address;
+    protected ArrayList<Integer> i;
+    protected int c;
 
-    protected void tryClearAllMessages(){
-        smsDb.deleteAllMessagesByRecipientId(address);
+    protected void setUpMessageIds(){
+        i = new ArrayList();
+        i.add(111);
+        i.add(222);
+        i.add(333);
+        when(smsDb.getMessageIdsByRecipientId(address)).thenReturn(i);
     }
 
     protected void setUpMessageCount(){
-        when(threadDb.getMessageCountByRecipientId(address)).thenReturn(10);
+        c = smsDb.getMessageIdsByRecipientId(address).size();
+        when(threadDb.getMessageCountByRecipientId(address)).thenReturn(c);
     }
 
 }
--- test/unitTest/java/org/thoughtcrime/securesms/database/MessageTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/database/MessageTest.java
@@ -21,17 +21,28 @@ public class MessageTest extends MessageMocking {
         threadDb = mock(ThreadDatabase.class);
         address = mock(Address.class);
 
+        super.setUpMessageIds();
         super.setUpMessageCount();
     }
 
     @Test
+    public void testGetMessageIdsByRecipientId(){
+        System.out.println("Testing Message IDs; Expected: [111,222,333], Actual: " + smsDb.getMessageIdsByRecipientId(address).toString());
+        assertEquals("[111, 222, 333]", smsDb.getMessageIdsByRecipientId(address).toString());
+    }
+
+    @Test
     public void testGetMessageCountByRecipientId(){
-        System.out.println("Testing Message Count #1; Expected: 10, Actual: " + threadDb.getMessageCountByRecipientId(address));
-        assertEquals(10, threadDb.getMessageCountByRecipientId(address));
-        System.out.println("All messages cleared.");
-        super.tryClearAllMessages();
+        System.out.println("Testing Message Count #1; Expected: 3, Actual: " + threadDb.getMessageCountByRecipientId(address));
+        assertEquals(3, threadDb.getMessageCountByRecipientId(address));
+    }
+
+    @Test
+    public void testDeleteAllMessagesByRecipientId(){
+        System.out.println("Clearing messages and testing message count again...");
+        smsDb.deleteAllMessagesByRecipientId(address);
         System.out.println("Testing Message Count #2; Expected: 0, Actual: " + threadDb.getMessageCountByRecipientId(address));
-        assertEquals(10, threadDb.getMessageCountByRecipientId(address));
+        assertEquals(0, threadDb.getMessageCountByRecipientId(address));
     }
 
 }
