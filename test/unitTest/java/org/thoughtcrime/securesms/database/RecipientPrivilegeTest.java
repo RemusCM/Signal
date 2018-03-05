5587c17ba09894cb9385cc76495b7dd97f97c207
daanish93 <daanishrehman93@gmail.com>
2018-03-05 18:39:02 -0500

    #49 Completed Passing and Failing Test for PermissionType.

--- test/unitTest/java/org/thoughtcrime/securesms/database/RecipientPrivilegeTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/database/RecipientPrivilegeTest.java
@@ -31,4 +31,13 @@ public class RecipientPrivilegeTest {
     assertEquals(PermissionType.CLEAR_GROUP_CONVERSATION.getPermissionTypeCode(), permissions[1]);
 
   }
+
+  @Test
+  public void testGetPermissionTypeCodeFail() {
+    //This test is expected to fail
+    String[] permissions = {"34", "47"};
+    assertEquals(PermissionType.EDIT_GROUP.getPermissionTypeCode(), permissions[0]);
+    assertEquals(PermissionType.CLEAR_GROUP_CONVERSATION.getPermissionTypeCode(), permissions[1]);
+
+  }
 }
