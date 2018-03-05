985965312114c7440830c2e739383dcb90eedbc7
Raymart De Guzman <raymart54@gmail.com>
2018-03-05 17:27:38 -0500

    #49 permission database unit test

---
+++ test/unitTest/java/org/thoughtcrime/securesms/database/PermissionDatabaseTest.java
@@ -0,0 +1,42 @@
+package org.thoughtcrime.securesms.database;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.List;
+
+@RunWith(JUnit4.class)
+public class PermissionDatabaseTest {
+
+  @Test
+  public void testGetRecipientPrivilegesString(String localNumber, String groupId) {
+
+  }
+
+  @Test
+  public void testSplitPrivilegesIntoList(String privileges) {
+
+  }
+
+  @Test
+  public void testHasEditGroupPermission(String localNumber, String groupId) {
+
+  }
+
+  @Test
+  public void testHasClearGroupChatPermission(String localNumber, String groupId) {
+
+  }
+
+  @Test
+  public void testCreate(String groupId, String moderator, String[] givenPrivileges, List members) {
+
+  }
+
+  @Test
+  public void testCreateUsingObject(PermissionDatabase.PermissionRecord record, List members) {
+
+  }
+
+}
