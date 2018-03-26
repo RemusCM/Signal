9f2622fa6cf833666307e55735f23603838ad05c
Raymart De Guzman <raymart54@gmail.com>
2018-03-26 15:15:00 -0400

    ref #62 minor fixes

--- src/org/thoughtcrime/securesms/ConversationListFragment.java
+++ src/org/thoughtcrime/securesms/ConversationListFragment.java
@@ -333,6 +333,7 @@ public class ConversationListFragment extends Fragment
     builder.setMessage(R.string.passcode_lock_confirmation__dialog);
     builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
       public void onClick(DialogInterface dialog, int id) {
+        /*
         Intent intent = new Intent(getActivity(), PasscodeActivity.class);
         List<Long> threadIds = new ArrayList<>();
         if (!selectedConversations.isEmpty()) {
@@ -341,6 +342,7 @@ public class ConversationListFragment extends Fragment
         String threadStr = String.valueOf(threadIds.get(0));
         intent.putExtra(PasscodeActivity.THREAD_ID, threadStr);
         startActivity(intent);
+        */
       }
     });
     builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
