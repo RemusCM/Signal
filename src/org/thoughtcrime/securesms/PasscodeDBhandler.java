7979667a439b3444e93f509484722903bbb313e1
Raymart De Guzman <raymart54@gmail.com>
2018-03-27 11:37:17 -0400

    #76 skeleton for PasscodeDBHandler

---
+++ src/org/thoughtcrime/securesms/PasscodeDBhandler.java
@@ -0,0 +1,91 @@
+package org.thoughtcrime.securesms;
+
+import android.content.Context;
+
+public class PasscodeDBhandler {
+  private static final String TAG = PasscodeDBhandler.class.getSimpleName();
+
+  private static final String PASSCODE = "passcode";
+
+  private Context context;
+  private long threadId;
+  private String passcode;
+
+  public String getPasscode() {
+    return passcode;
+  }
+
+  public long getThreadId() {
+    return threadId;
+  }
+
+  PasscodeDBhandler(Context context, long threadId, String passcode) {
+    this.context = context;
+    this.threadId = threadId;
+    this.passcode = passcode;
+  }
+
+  PasscodeDBhandler(Context context, long threadId) {
+    this.context = context;
+    this.threadId = threadId;
+  }
+
+  /**
+   * Given a threadId and a passcode,
+   * get the passcode for this thread
+   * if it exists
+   * @return the passcode currently
+   * stored in the database
+   */
+  public String getPasscodeIfExists() {
+    // TODO
+    return "";
+  }
+
+  /**
+   * Given a threadId and passcode,
+   * check if passcode field is set
+   * in the thread database
+   * @return true if passcode exists for this thread
+   */
+  public boolean isPasscodeExists() {
+    return !getPasscodeIfExists().isEmpty() || getPasscodeIfExists() != null;
+  }
+
+  /**
+   * Given a threadId and a passcode,
+   * update the passcode field
+   * in the thread database
+   * @return updates the passcode field
+   */
+  public String update() {
+    String currentPasscode = getPasscodeIfExists();
+    // checks if you enter the correct passcode
+    if (getPasscode().equals(currentPasscode)) {
+      /*DatabaseFactory.getPasscodeDatabase(context).updatePasscode(threadId, passcode);
+      if (DatabaseFactory.getPasscodeDatabase(context).isUpdated(threadId, passcode)) {
+        return "Success";
+      }*/
+    }
+    return "Error";
+  }
+
+  /**
+   * Given a threadId and a passcode,
+   * remove the passcode stored in
+   * the passcode field in the
+   * thread database
+   * @return removes the passcode stored
+   * in the field
+   */
+  public String delete() {
+    String currentPasscode = getPasscodeIfExists();
+    if (getPasscode().equals(currentPasscode)) {
+      /*DatabaseFactory.getPasscodeDatabase(context).removePasscode(threadId, passcode);
+      if (DatabaseFactory.getPasscodeDatabase(context).isUpdated(threadId, passcode)) {
+        return "Success";
+      }*/
+    }
+    return "Error";
+  }
+}
