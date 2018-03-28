67c5a10d1dac171c17a797d89c68666ab3234467
daanish93 <daanishrehman93@gmail.com>
2018-03-28 14:15:03 -0400

    #69 #75 Added indicator for locked conversation.

--- src/org/thoughtcrime/securesms/ConversationListItem.java
+++ src/org/thoughtcrime/securesms/ConversationListItem.java
@@ -26,7 +26,9 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -74,6 +76,7 @@ public class ConversationListItem extends RelativeLayout
   private AlertView          alertView;
   private ImageView          unreadIndicator;
   private long               lastSeen;
+  private ImageButton        lockButton;
 
   private int             unreadCount;
   private AvatarImageView contactPhotoImage;
@@ -101,6 +104,7 @@ public class ConversationListItem extends RelativeLayout
     this.thumbnailView           = findViewById(R.id.thumbnail);
     this.archivedView            = findViewById(R.id.archived);
     this.unreadIndicator         = findViewById(R.id.unread_indicator);
+    this.lockButton              = findViewById(R.id.lock_button);
     thumbnailView.setClickable(false);
 
     ViewUtil.setTextViewGravityStart(this.fromView, getContext());
@@ -144,6 +148,13 @@ public class ConversationListItem extends RelativeLayout
     setRippleColor(recipient);
     setUnreadIndicator(thread);
     this.contactPhotoImage.setAvatar(glideRequests, recipient, true);
+
+    PasscodeDBhandler handler = new PasscodeDBhandler(getContext(), threadId);
+    Log.i(TAG, "handler.isPasscodeExists()" + handler.isPasscodeExists());
+    if(handler.isPasscodeExists()){
+      lockButton.setVisibility(View.VISIBLE);
+    }
+
   }
 
   @Override
