164ead1f877956c3438bebaa67571b5501c03ed1
daanish93 <daanishrehman93@gmail.com>
2018-04-04 15:01:29 -0400

    #99 #95 Added drawing setup mechanism.

--- test/unitTest/java/org/thoughtcrime/securesms/DrawingTestingMocking.java
+++ test/unitTest/java/org/thoughtcrime/securesms/DrawingTestingMocking.java
@@ -1,5 +1,8 @@
 package org.thoughtcrime.securesms;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.widget.Button;
 
 import org.junit.runner.RunWith;
@@ -19,13 +22,46 @@ public class DrawingTestingMocking extends BaseUnitTest{
   private Button drawBtn;
   private Button eraseBtn, saveBtn;
 
+  private CustomPath drawPath;
+  private Bitmap canvasBitmap;
+  private Paint drawPaint;
+  private Paint canvasPaint;
+  private int paintColor = 000000;
+  private Canvas drawCanvas;
+
+  private float brushSize;
+  private float prevBrushSize; 
+
+  private boolean erase = false;
+  public boolean smoothStrokes = false;
+
+  private float startX;
+  private float startY;
+  private float endX;
+  private float endY;
+
   @Override
-  public void setUp(){
+  public void setUp() {
     drawView = mock(DrawingView.class);
     drawBtn = mock(Button.class);
     eraseBtn = mock(Button.class);
     saveBtn = mock(Button.class);
 
+  }
+
+  public void setUpDrawing(){
+    drawPaint = new Paint();;
+    drawPath = new CustomPath(paintColor, brushSize);
+    erase = false;
+    smoothStrokes = false;
+    drawPaint.setColor(paintColor);
+    drawPaint.setAntiAlias(true);
+    drawPaint.setStyle(Paint.Style.STROKE);
+    drawPaint.setStrokeJoin(Paint.Join.ROUND);
+    drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    canvasPaint = new Paint(Paint.DITHER_FLAG);
+    brushSize = 20;
+    prevBrushSize = brushSize;
   }
 
 
